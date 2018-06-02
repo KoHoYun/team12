1dad379d96054185d7f7d3192827df51623e8c2a
seora <tjfk4938@hanmail.net>
2018-06-02 15:29:16 +0900

    Add files via upload

---
+++ Simple_M/src/SimpleM.java
@@ -0,0 +1,9 @@
+
+public class SimpleM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Simple_M/src/controllers/Edit_control.java
@@ -0,0 +1,5 @@
+package controllers;
+
+public class Edit_control {
+
+}
