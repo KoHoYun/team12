ce6c7463f8a109f990774dbf6b045f8bc0e4b40e
Lilian <liver@cau.ac.kr>
2018-06-02 21:27:04 +0900

    Update MainControl.java

--- Simple_M/src/controllers/MainControl.java
+++ Simple_M/src/controllers/MainControl.java
@@ -2,10 +2,15 @@ package controllers;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 
 import javax.swing.JFileChooser;
 import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Utilities;
 
 import models.*;
 import views.*;
@@ -14,6 +19,9 @@ public class MainControl {
 	
 	private View theView;
 	private OpenFile theFile;
+	
+	int interpos = 1;
+
 	
 	public MainControl(View theView) {
 		this.theView = theView;
@@ -54,7 +62,6 @@ public class MainControl {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				new Save_control(theView.saveBtn1, getTextLeft(),e);
-				theView.loadBtn1.setEnabled(true);
 			}
 		});
 		
@@ -64,22 +71,62 @@ public class MainControl {
 			public void actionPerformed(ActionEvent e) {
 				
 				new Save_control(theView.saveBtn2, getTextRight(),e);
-				theView.loadBtn2.setEnabled(true);
 			}
 		});
+		
 		
 		 this.theView.compareBtn.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					try {
 						new Compare_control(getTextLeft(), getTextRight());
-						// mergeText
+						
+						// mergeText 					
+						
+						
+						
+						// get clicked line number from leftcode
+						theView.leftcode.addMouseListener(new MouseAdapter() {
+							
+							@Override
+							public void mousePressed(MouseEvent e) {
+								interpos = theView.leftcode.getCaretPosition();
+								// System.out.println("* interpos: "+interpos);
+							}
+							
+						});
+							
+						
+						
+						// 1. right to left
+						theView.mergeBtn1.addActionListener(new ActionListener() {
+							@Override
+							public void actionPerformed(ActionEvent e) {
+								// System.out.println("left interpos: "+interpos);
+								new Merge_control(theView.leftcode, theView.rightcode, interpos, 0);
+							}
+
+							
+						});
+						
+						// 2. left to right
+						theView.mergeBtn2.addActionListener(new ActionListener() {
+							@Override
+							public void actionPerformed(ActionEvent e) {
+								// System.out.println("right interpos: "+interpos);
+								new Merge_control(theView.leftcode, theView.rightcode, interpos, 1);
+								
+							}
+
+						});
+						
+						
 					} catch (IOException event) {
 						return;
 					} catch (Exception e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
-					//theView.compareBtn.setEnabled(false);
+					// theView.compareBtn.setEnabled(false);
 				}
 			});
 	}
