6888ffa792ed492b13b3e48764a7c0a22c491624
dlaalswjd <rladidtla70@naver.com>
2018-06-02 17:02:57 +0900

    Update Identical_control.java

--- Simple_M/src/controllers/Identical_control.java
+++ Simple_M/src/controllers/Identical_control.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import views.*;
 import models.*;
 import views.*;
 
@@ -9,24 +10,9 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class Identical_control {
-	
-	private JFrame jframe = new JFrame();
-	private JButton OK_button = new JButton("OK");
-	private JLabel label = new JLabel("Two files are identical");
-	public Identical_control()
+	public Identical_control(boolean f)
 	{
-		jframe.add(label,"Center");
-		jframe.add(OK_button,"South");
-		
-		OK_button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				jframe.dispose();
-			}
-		});
-		
-		jframe.setSize(200, 200);
-		jframe.setVisible(true);
-		jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE);
+		new Identical(f);
 	}
 
 }
