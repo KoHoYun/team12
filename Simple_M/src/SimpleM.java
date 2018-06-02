8aeaad79107bac6eeb884f85238a32059c00efa9
seora <tjfk4938@hanmail.net>
2018-06-02 15:45:08 +0900

    Update SimpleM.java

--- Simple_M/src/SimpleM.java
+++ Simple_M/src/SimpleM.java
@@ -1,8 +1,15 @@
+import javax.swing.JFrame;
+
+import controllers.*;
+import models.*;
+import views.*;
 
 public class SimpleM {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		new MainControl(new View());
 
 	}
 
