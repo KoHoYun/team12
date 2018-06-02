d59ea4d24bb4a02bbe759b0ad680385376c23805
Youngeun Kim <33536656+YoungeuNNN@users.noreply.github.com>
2018-06-03 04:55:21 +0900

    Update Merge_control.java

--- Simple_M/src/controllers/Merge_control.java
+++ Simple_M/src/controllers/Merge_control.java
@@ -1,53 +1,131 @@
 package controllers;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import javax.swing.JTextPane;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 import javax.swing.text.Element;
+import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.Utilities;
 
 
 public class Merge_control {
-	
-		public Merge_control() {
-		
+	public Merge_control() {
 	}
+	public Merge_control(JTextPane first, JTextPane second,int pos,int i) throws BadLocationException {
+		
+		int lineNum = getLineCount(first,pos);	
+		System.out.println("Successfully Construct Merge_control*");
+		lineNum = getLineCount(first,pos);
+		
+		if(i == 0)
+			MergetoLeft(first,second,lineNum);
+		else
+			MergetoRight(first,second,lineNum);
+		
+		
+		 
+	}	
+
 	
-		
-		
-	public Merge_control(JTextPane first, JTextPane second,int pos){
+	
+	public void MergetoRight(JTextPane first,JTextPane second,int lineNum) throws BadLocationException {
+		System.out.println("Enter the MergetoLeft*");
 		Document doc1 = first.getStyledDocument();
-		Document doc2 = second.getStyledDocument();
-		
+		Document doc2 = second.getStyledDocument();		
 		Element root1 = doc1.getDefaultRootElement();
 		Element root2 = doc2.getDefaultRootElement();
 		
-		lineNum = getLineCount(first,pos);
+		ArrayList<String> repLine = new ArrayList<String>();
+		ArrayList<Color> ColorSet = new ArrayList<Color>();
 		
-		//Element text1 = root1.getElement(getLineCount()-1);	
-	}
-	
-	public void MergetoLeft(Element root1, Element root2,int lineNum) {
-		Element text1 = root1.getElement(lineNum);//linenum에 맞게 추출되는지 확인 
-		Color colr = StyleConstants.getBackground(text1.getAttributes()); //color제대로 뽑히는지 확인
-		int count = 0;
-		int start=0, end=0;
-		while(validColor(colr)) {
-			
-			if()
-			
+		Element text1 = root1.getElement(lineNum-1);//linenum에 맞게 추출되는지 확인
+		for(int j=0;;j++) {//Colorset만드려고
+			Color Colr = getColor(text1);
+			if((Colr == Color.ORANGE) || (Colr == Color.YELLOW) ||(Colr == Color.GRAY)) {
+			ColorSet.add(Colr);
+			text1 = root1.getElement(lineNum-1+j);//다음 엘리먼트 
+			}
+			else
+				break;
+		}//colorset저장 size구하면 라인수 알 수 있음
+		
+		
+		
+		Element text2 = root2.getElement(lineNum-1);
+		
+		
+		
+		int start1=text1.getStartOffset(), end1=text1.getEndOffset();
+		int start2=0, end2=0;
+		
+		//컬러 맞으면 반복
+		try {
+			repLine.add(doc1.getText(start1, end1-start1));
+		} catch (BadLocationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		//repLine에 복사해야 하는 텍스트 저장
+		
+		boolean gray = false;
+		for(int j=0;j<ColorSet.size();j++)
+		{
+			if(ColorSet.get(j)==Color.GRAY)
+				gray = true;
+		}
+		
+		
+		
+		
+		
+		
+		
+		///아래의 코드는 노랑색만 가능 (gray == false일때)
+		if(repLine.size()==1)
+		{
+			start2 = text2.getStartOffset();
+			end2 = text2.getEndOffset();
+		}
+		else if(repLine.size()>1)
+		{
+			start2 = text2.getStartOffset();//오른쪽꺼 시작 오프셋 구하기
+			text2 = root2.getElement(lineNum-1+repLine.size()-1);//마지막 줄lineNum-1 => lineNum에 해당하는 element구하기 element는 0시작하니까, repLine.size()-1는 마지막줄 나타내기 위해서
+			end2 = text2.getEndOffset(); //마지막 줄 엔드오프셋
+		}
+		
+		doc2.remove(start2,end2); //doc에서 제거 
+		
+		Style style = first.addStyle("String", null);
+		for(int j=repLine.size();j>0;j--) { //1번부터 넣으면 offset이 바껴서 못찾음
+		doc2.insertString(start2,repLine.get(j),style);
+		}
+		//회색이면 회색인 오프셋 받아서 삭제하면됨
+		
+		
+	
+		
+		
+		//end1 = root1.getEndOffset();
 			
 		//lineElem = elem.getElement(lineCount);
 		
 	}
-	public void MergetoRight() {
+	public void MergetoLeft(JTextPane first,JTextPane second,int lineNum) {
+		Document doc1 = first.getStyledDocument();
+		Document doc2 = second.getStyledDocument();		
+		Element root1 = doc1.getDefaultRootElement();
+		Element root2 = doc2.getDefaultRootElement();
+		//pos넣어서 getElementIndex 해보기 아마 될듯
+	
+		
+		ArrayList<String> repLine = new ArrayList<String>();
 	}
-	//지효 쓰라하기 
+	
 	public int getLineCount(JTextPane textPane, int pos)
 	{
 		int lineCount = (pos==0) ? 1 : 0;
@@ -60,7 +138,7 @@ public class Merge_control {
         } catch (BadLocationException e) {
             e.printStackTrace();
         }
-        System.out.println("라인넘버는" + lineCount);
+        System.out.println("선택된 라인은" + lineCount);
         return lineCount;
         //line rn(row number) = 클릭된줄넘버 저장 
         
@@ -76,8 +154,9 @@ public class Merge_control {
         System.out.println("라인넘버는" + lineCount);
         return lineCount;
     }
-	
-	public boolean validColor(Color colr) {
+	public boolean validColor(Element elem) {
+		
+		Color colr = getColor(elem);
 		if((colr== Color.YELLOW) || (colr== Color.ORANGE) ||(colr== Color.GRAY))
 		{
 			return true;
@@ -85,6 +164,17 @@ public class Merge_control {
 		else
 		{
 			return false;
+		}
+	}
+	public Color getColor(Element elem) {//++++++++++++찌효 만들어주세영
+		Color colr = Color.YELLOW;
+		if((colr== Color.YELLOW) || (colr== Color.ORANGE) ||(colr== Color.GRAY))
+		{
+			return colr;
+		}
+		else
+		{
+			return colr;
 		}
 	}
 }
