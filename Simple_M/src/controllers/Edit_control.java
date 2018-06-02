25e1910ba21d7658a811edf0aa1686924c3e8ea9
seora <tjfk4938@hanmail.net>
2018-06-02 15:51:09 +0900

    Update Edit_control.java

--- Simple_M/src/controllers/Edit_control.java
+++ Simple_M/src/controllers/Edit_control.java
@@ -1,5 +1,18 @@
 package controllers;
 
+import models.*;
+import views.*;
+
+import javax.swing.JButton;
+import javax.swing.JTextPane;
+
 public class Edit_control {
+	
+	 /* edit button을 누르면 textpane이 수정가능해지고, load button은 누를 수 없어서 text file을 불러올 수 없다 */
+	
+	public Edit_control(JTextPane a, JButton aButton, boolean editable, boolean push) {
+		a.setEditable(editable);
+		aButton.setEnabled(push);
+	}
 
 }
