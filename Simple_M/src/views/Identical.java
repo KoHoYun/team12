b395745aec825d29c8637d3e35c855183c927994
dlaalswjd <rladidtla70@naver.com>
2018-06-02 17:01:55 +0900

    Update Identical.java

--- Simple_M/src/views/Identical.java
+++ Simple_M/src/views/Identical.java
@@ -1,5 +1,29 @@
 package views;
 
-public class Identical {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Identical {
+	private JFrame jframe = new JFrame();
+	private JButton OK_button = new JButton("OK");
+	private JLabel label = new JLabel("Two files are identical");
+	public Identical(boolean bool)
+	{
+		jframe.add(label,"Center");
+		jframe.add(OK_button,"South");
+		
+		OK_button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				jframe.dispose();
+			}
+		});
+		
+		jframe.setSize(200, 200);
+		jframe.setVisible(bool);
+		jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE);
+	}
 }
