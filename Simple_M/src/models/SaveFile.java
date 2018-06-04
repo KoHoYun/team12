666803f3814f9a3a166bef4b605894417fb7da95
dlaalswjd <rladidtla70@naver.com>
2018-06-05 00:52:11 +0900

    Update SaveFile.java

--- Simple_M/src/models/SaveFile.java
+++ Simple_M/src/models/SaveFile.java
@@ -1,5 +1,31 @@
 package models;
 
-public class SaveFile {
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileWriter;
 
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JTextPane;
+
+public class SaveFile {
+	public SaveFile(JButton button, JTextPane text, ActionEvent e) {
+		if (e.getSource() == button) {
+			JFileChooser sf = new JFileChooser();
+			if (sf.showSaveDialog(button) == sf.APPROVE_OPTION) {
+				try {
+					String str = text.getText().trim();
+					if (str.length() < 1)
+						return;
+					File f = sf.getSelectedFile();
+					FileWriter fw = new FileWriter(f);
+					fw.write(str);
+					fw.close();
+
+				} catch (Exception ex) {
+				}
+			}
+
+		}
+	}
 }
