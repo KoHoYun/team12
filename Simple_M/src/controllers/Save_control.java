f3e08f256d0ced424cf1b7031683ac94a6d11770
dlaalswjd <rladidtla70@naver.com>
2018-06-05 00:51:41 +0900

    Update Save_control.java

--- Simple_M/src/controllers/Save_control.java
+++ Simple_M/src/controllers/Save_control.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.*;
 import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.FileWriter;
@@ -13,24 +14,9 @@ import views.*;
 
 public class Save_control {
 
-	public Save_control(JButton button, JTextPane text, ActionEvent e) {
-		if (e.getSource() == button) {
-			JFileChooser sf = new JFileChooser();
-			if (sf.showSaveDialog(button) == sf.APPROVE_OPTION) {
-				try {
-					String str = text.getText().trim();
-					if (str.length() < 1)
-						return;
-					File f = sf.getSelectedFile();
-					FileWriter fw = new FileWriter(f);
-					fw.write(str);
-					fw.close();
-
-				} catch (Exception ex) {
-				}
-			}
-
-		}
+	public Save_control(JButton button, JTextPane text, ActionEvent e)
+	{
+		new SaveFile(button, text, e);
 	}
 	
 }
