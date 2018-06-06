a9ef21328d87b6ca26f6cacf6a64072689b3362b
Youngeun Kim <33536656+YoungeuNNN@users.noreply.github.com>
2018-06-06 21:22:29 +0900

    Update Edit_control.java

--- Simple_M/src/controllers/Edit_control.java
+++ Simple_M/src/controllers/Edit_control.java
@@ -8,7 +8,7 @@ import javax.swing.JTextPane;
 
 public class Edit_control {
 	
-	 /* edit button�쓣 �늻瑜대㈃ textpane�씠 �닔�젙媛��뒫�빐吏�怨�, load button�� �늻瑜� �닔 �뾾�뼱�꽌 text file�쓣 遺덈윭�삱 �닔 �뾾�떎 */
+	 /*text area can editable*/
 	
 	public Edit_control(JTextPane a, boolean editable) {
 		a.setEditable(editable);
